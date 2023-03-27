ddb88336d0a2a41a14eb84b0f54d164b268883e3
Matt Wanner <wanne051@morris.umm.edu>
2023-03-27 12:33:33 -0500

    #14 adds item class and methods for adding and accessing items from db
    Co-authored-by: mason-eisch <eisch115@morris.umn.edu>

---
+++ server/src/main/java/umm3601/item/Item.java
@@ -0,0 +1,32 @@
+package umm3601.item;
+
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
+
+public class Item {
+    @ObjectId @Id
+
+  @SuppressWarnings({"MemberName"})
+
+  public String _id;
+
+  public String itemName;
+  public int amount;
+  public String unit;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Item)) {
+      return false;
+    }
+    Item other = (Item) obj;
+    return _id.equals((other._id));
+  }
+
+  @Override
+  public int hashCode() {
+    return _id.hashCode();
+  }
+}
