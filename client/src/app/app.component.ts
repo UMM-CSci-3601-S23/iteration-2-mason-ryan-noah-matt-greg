6f8cdb611a81be4c44462ab7b9a3f81dd8cb52a4
Mason Eischens <eisch115@morris.umn.edu>
2023-04-04 14:44:05 -0500

    #13 Removes sidenav, replaces it with horizontal nav bar with dynamic contnt

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router,NavigationEnd  } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'PocketShelf';
+  currentRoute: string;
+
+  constructor(private router: Router){
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd && event.url) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+
 }
