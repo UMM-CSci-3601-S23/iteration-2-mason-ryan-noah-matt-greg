418b24e0f238f6ede274fee9f7622630af1e80b3
Ryan Sajulga <sajul004@morris.umn.edu>
2023-04-06 15:32:37 -0500

    Tests Inventory Component
    Creating item.service.mock and item.spec is passing
    Saving progress so Mason can check his coverage
    Co-authored-by: Matt Wanner <wanne051@morris.umn.edu>

--- client/src/app/inventory/item.service.ts
+++ client/src/app/inventory/item.service.ts
@@ -41,35 +41,3 @@ export class ItemService {
     return this.httpClient.post<{ id: string }>(this.newItemUrl, newItem).pipe(map(res => res.id));
   }
 }
-
-  // readonly itemUrl: string = environment.apiUrl + 'items';
-  // constructor(private httpClient: HttpClient) {
-  // }
-
-  // getItems(filters?: { itemName?: string; unit?: string; amount?: string }): Observable<Item[]> {
-
-  //   let httpParams: HttpParams = new HttpParams();
-  //   if (filters) {
-  //     if (filters.itemName) {
-  //       httpParams = httpParams.set('itemName', filters.itemName);
-  //     }
-  //     if (filters.unit) {
-  //       httpParams = httpParams.set('unit', filters.unit);
-  //     }
-  //     if (filters.amount) {
-  //       httpParams = httpParams.set('amount', filters.amount);
-  //     }
-  //   }
-  //   return this.httpClient.get<Item[]>(this.itemUrl, {
-  //     params: httpParams,
-  //   });
-  // }
-
-  // getItemById(id: string): Observable<Item>{
-  //   return this.httpClient.get<Item>(this.itemUrl +'/' + id);
-  // }
-
-  // addItem(newItem: Item): Observable<string> {
-  //   return this.httpClient.post<{id: string}>(this.itemUrl, newItem).pipe(map(res => res.id));
-  // }
-
